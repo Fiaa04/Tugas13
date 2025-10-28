c5145cdd15efd189e56d984551d8fcd3c5b2068e
Lutfianti Aurelia Putri <fiaaurelia04@gmail.com>
2025-10-28 19:49:19 +0700

    fiksss

--- config/config.php
+++ config/config.php
@@ -1,14 +1,14 @@
 <?php
 $host = "localhost";
-$user = "xirpl1-11"; 
-$pass = "0098375178";        
-$db   = "db_xirpl1-11_1"; 
+$user = "xirpl1-11";
+$pass = "0098375178"; 
+$db   = "db_xirpl1-11_1";
 
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 
 if (!$koneksi) {
-    die("Koneksi database gagal: " . mysqli_connect_error());
-}else {
-    // echo "Koneksi berhasil"; // aktifkan sementara untuk test
+    die("❌ Koneksi database gagal: " . mysqli_connect_error());
+} else {
+    //echo "✅ Koneksi berhasil ke database!";
 }
 ?>
