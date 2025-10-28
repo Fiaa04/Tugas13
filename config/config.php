69d55a758a8c65eb7db83cb7f34c411136ce27fc
Lutfianti Aurelia Putri <fiaaurelia04@gmail.com>
2025-10-28 19:28:04 +0700

    komitan3

--- config/config.php
+++ config/config.php
@@ -1,12 +1,12 @@
 <?php
 $host = "localhost";
 $user = "xirpl1-11"; 
-$pass = "0098375178";  
+$pass = "0098375178";        
 $db   = "db_xirpl1-11_1"; 
 
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 
 if (!$koneksi) {
-    die("Koneksi gagal: " . mysqli_connect_error());
+    die("Koneksi database gagal: " . mysqli_connect_error());
 }
 ?>
