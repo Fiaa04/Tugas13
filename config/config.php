d2abff1d48f50b1684fc842e3f48fe28cbc56fc4
Lutfianti Aurelia Putri <fiaaurelia04@gmail.com>
2025-10-28 19:57:51 +0700

    1

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
-$user = "root";
-$pass = ""; 
+$user = "rxirpl1-11";
+$pass = "0098375178"; 
 $db   = "db_xirpl1-11_1";
 
 $koneksi = mysqli_connect($host, $user, $pass, $db);
